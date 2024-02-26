4deff5550cf0b02ab1f3e9d290652d7580e2e19d
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2024-02-26 15:24:33 +0200

    Initial Commit

---
+++ Soosu/SoosuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoosuApp.swift
+//  Soosu
+//
+//  Created by Ahmed Shendy on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SoosuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
